d9bca88946c8b6c61d9403515291ca9b628cea23
Niranjan <malakaniranjan080@gmail.com>
2025-10-02 14:04:19 +0530

    Update ShoppingCartRepository.java

--- tshirt-store/src/main/java/com/store/tshirt_store/ShoppingCartRepository.java
+++ tshirt-store/src/main/java/com/store/tshirt_store/ShoppingCartRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface ShoppingCartRepository extends JpaRepository<ShoppingCart, Long> {
-    // A magic method to find a cart by the user who owns it
+    
     Optional<ShoppingCart> findByUser(User user);
 }
