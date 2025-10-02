8e712841aef18e93702e67019b432510f4b7c71d
Niranjan <malakaniranjan080@gmail.com>
2025-10-02 14:02:32 +0530

    Update UserRepository.java

--- tshirt-store/src/main/java/com/store/tshirt_store/UserRepository.java
+++ tshirt-store/src/main/java/com/store/tshirt_store/UserRepository.java
@@ -1,11 +1,9 @@
 package com.store.tshirt_store;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional; // <-- Add this import
+import java.util.Optional; 
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    // This is a magic method! Spring Data JPA will automatically create the code
-    // to search for a user by their username.
     Optional<User> findByUsername(String username);
 }
